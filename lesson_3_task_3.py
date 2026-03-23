eb911792cdbfe3689f336dfa6d4cca82c33aa983
BubbleTv <vladasiskina545@gmail.com>
2026-03-23 19:15:48 +0300

    Add files via upload

--- lesson_3_task_3.py
+++ lesson_3_task_3.py
@@ -1,7 +1,7 @@
 from address import Address
 from mailing import Mailing
 
-
+# Создаем адрес отправителя
 from_address = Address(
     "123456",
     "Москва",
@@ -10,7 +10,7 @@ from_address = Address(
     "78"
 )
 
-
+# Создаем адрес получателя
 to_address = Address(
     "654321",
     "Санкт-Петербург",
@@ -19,7 +19,7 @@ to_address = Address(
     "42"
 )
 
-
+# Создаем экземпляр класса Mailing
 mailing = Mailing(
     to_address=to_address,
     from_address=from_address,
@@ -27,7 +27,7 @@ mailing = Mailing(
     track="TRK123456789"
 )
 
-
+# Распечатываем информацию об отправлении
 print(f"Отправление {mailing.track} из {mailing.from_address.index}, "
       f"{mailing.from_address.city}, {mailing.from_address.street}, "
       f"{mailing.from_address.house} - {mailing.from_address.apartment} в "
