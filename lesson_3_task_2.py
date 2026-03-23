e8c1253f88db7bb3bf2c91717d367e57a86dc01f
BubbleTv <vladasiskina545@gmail.com>
2026-03-23 13:35:51 +0300

    Add files via upload

---
+++ lesson_3_task_2.py
@@ -0,0 +1,9 @@
+from smartphone import Smartphone
+catalog = []
+catalog.append(Smartphone("Apple", "iPhone 15 Pro", "+79123456789"))
+catalog.append(Smartphone("Samsung", "Galaxy S24 Ultra", "+79234567890"))
+catalog.append(Smartphone("Xiaomi", "Mi 14 Pro", "+79345678901"))
+catalog.append(Smartphone("Google", "Pixel 9 Pro", "+79456789012"))
+catalog.append(Smartphone("OnePlus", "12", "+79567890123"))
+for smartphone in catalog:
+    print(f"{smartphone.brand} - {smartphone.model}. {smartphone.phone_number}")
